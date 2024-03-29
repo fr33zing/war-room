4ddb6a23198a057c104bf0e7912a526f30a5ea0b
fr33zing <fr33zing@none.none>
2024-03-29 16:58:48 -0700

    add multi-platform devShell stub

--- flake.nix
+++ flake.nix
@@ -1,15 +1,33 @@
 {
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.war-room = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = let settings = import ./settings.nix;
-      in { inherit (settings) domain email; };
-      modules = [
-        ./configuration.nix
-        ./modules/shell.nix
-        ./modules/matrix-conduit.nix
-        ./modules/nginx.nix
-      ];
-    };
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs"; };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      inherit (nixpkgs) lib;
+
+      settings = import ./settings.nix;
+
+      withSystem = f:
+        lib.fold lib.recursiveUpdate { } (map (s: f s) [
+          "x86_64-linux"
+          "x86_64-darwin"
+          "aarch64-linux"
+          "aarch64-darwin"
+        ]);
+    in ({
+      nixosConfigurations.war-room = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit (settings) domain email; };
+        modules = [
+          ./configuration.nix
+          ./modules/shell.nix
+          ./modules/matrix-conduit.nix
+          ./modules/nginx.nix
+        ];
+      };
+    } // withSystem (system: {
+      devShell.${system} = nixpkgs.mkShell {
+        # ...
+      };
+    }));
 }
