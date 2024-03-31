f0e08ed48fc5d2a92f97013ca920531aa3b9d96e
fr33zing <fr33zing@none.none>
2024-03-30 19:03:25 -0700

    add create-user tool

--- flake.nix
+++ flake.nix
@@ -25,9 +25,28 @@
           ./modules/nginx.nix
         ];
       };
-    } // withSystem (system: {
-      devShell.${system} = nixpkgs.mkShell {
-        # ...
-      };
-    }));
+    } // withSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        filesIn = dir:
+          builtins.attrNames
+          (lib.filterAttrs (n: v: v == "regular") (builtins.readDir dir));
+        tools = map (file:
+          pkgs.writeScriptBin file (builtins.readFile (./tools + ("/" + file))))
+          (filesIn ./tools);
+      in {
+        devShell.${system} = pkgs.mkShell {
+          packages = with pkgs; [ python3 ] ++ tools;
+          shellHook = ''
+            printf '\n%s\n' '[Commands]'
+          '' + (builtins.concatStringsSep "\n" (map (tool:
+            let
+              command = pkgs.lib.getExe tool;
+              name = builtins.baseNameOf command;
+            in ''
+              printf '\n%s: ' '${name}'
+              ${command} --describe
+            '') tools));
+        };
+      }));
 }
