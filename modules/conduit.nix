9af24361ec53e4bd16247acf612870360ab8e8bc
fr33zing <fr33zing@none.none>
2024-04-01 17:16:29 -0700

    set max request size

--- modules/conduit.nix
+++ modules/conduit.nix
@@ -4,6 +4,7 @@
     settings.global = {
       server_name = domain;
       database_backend = "rocksdb";
+      max_request_size = 1000000;
       allow_federation = true;
       allow_registration = false;
     };
