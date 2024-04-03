a9b449a6552c0f4a5ee7c9dfa407d3b95c761053
fr33zing <fr33zing@none.none>
2024-04-02 19:35:54 -0700

    add dendrite registration toggle

--- modules/dendrite.nix
+++ modules/dendrite.nix
@@ -1,4 +1,6 @@
-{ config, domain, ... }: {
+{ config, pkgs, domain, ... }:
+let inherit (pkgs) lib;
+in {
   services.postgresql.enable = true;
 
   users.groups.dendrite = { };
@@ -8,36 +10,44 @@
     group = "dendrite";
   };
 
-  services.dendrite = {
-    enable = true;
-    environmentFile = config.sops.secrets."dendrite_env".path;
+  services.dendrite = let DANGEROUS_ALLOW_REGISTRATION = false;
+  in lib.mkMerge [
+    {
+      enable = true;
+      environmentFile = config.sops.secrets."dendrite_env".path;
 
-    settings = {
-      global = {
-        server_name = domain;
-        private_key = config.sops.secrets."matrix_key".path;
-        database = {
-          connection_string = "$CONNECTION_STRING";
-          max_open_conns = 90;
-          max_idle_conns = 5;
-          conn_max_lifetime = -1;
+      settings = {
+        global = {
+          server_name = domain;
+          private_key = config.sops.secrets."matrix_key".path;
+          database = {
+            connection_string = "$CONNECTION_STRING";
+            max_open_conns = 90;
+            max_idle_conns = 5;
+            conn_max_lifetime = -1;
+          };
         };
+
+        client_api.registration_shared_secret = "$REGISTRATION_SHARED_SECRET";
+
+        # Disable sqlite databases
+        app_service_api.database.connection_string = "";
+        federation_api.database.connection_string = "";
+        key_server.database.connection_string = "";
+        media_api.database.connection_string = "";
+        mscs.database.connection_string = "";
+        relay_api.database.connection_string = "";
+        room_server.database.connection_string = "";
+        sync_api.database.connection_string = "";
+        user_api.database.connection_string = "";
+        user_api.account_database.connection_string = "";
+        user_api.device_database.connection_string = "";
       };
+    }
 
-      client_api.registration_shared_secret = "$REGISTRATION_SHARED_SECRET";
-
-      # Disable sqlite databases
-      app_service_api.database.connection_string = "";
-      federation_api.database.connection_string = "";
-      key_server.database.connection_string = "";
-      media_api.database.connection_string = "";
-      mscs.database.connection_string = "";
-      relay_api.database.connection_string = "";
-      room_server.database.connection_string = "";
-      sync_api.database.connection_string = "";
-      user_api.database.connection_string = "";
-      user_api.account_database.connection_string = "";
-      user_api.device_database.connection_string = "";
-    };
-  };
+    (lib.mkIf DANGEROUS_ALLOW_REGISTRATION {
+      openRegistration = true;
+      settings.client_api.registration_disabled = false;
+    })
+  ];
 }
